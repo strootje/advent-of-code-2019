442d808fcac754045526c841e3ba8b2e39edaa5a
Bastiaan Stroosnijder <bas@strootje.com>
2019-12-02 23:26:45 +0100

    added tests for star 3

--- stars/star3.sh
+++ stars/star3.sh
@@ -8,11 +8,21 @@ source ./stars/star1.sh 0
 
 # Variables
 INPUT_URL="https://adventofcode.com/2019/day/2/input"
-declare -A CODES=()
+declare -a CODES=()
+declare -a MEMORY=()
 COUNT=0
 
 get_codes() {
 	FILE=$1
+
+	parse_codes $(cat $FILE)
+}
+
+parse_codes() {
+	RAW=$1
+
+	COUNT=0
+	CODES=()
 
 	IFS=","
 	while read code; do
@@ -20,7 +30,56 @@ get_codes() {
 			CODES[$COUNT]=$i
 			COUNT=$(($COUNT + 1))
 		done
-	done < $FILE
+	done <<< $RAW
+	unset IFS
+}
+
+reset_memory() {
+	MEMORY=("${CODES[@]}")
+}
+
+run_program() {
+	OPCODE=$1
+	INUM1=$2
+	INUM2=$3
+	IRES=$4
+
+	LNUM1=${MEMORY[$INUM1]}
+	LNUM2=${MEMORY[$INUM2]}
+	LRES=${MEMORY[$IRES]}
+
+	VNUM1=${MEMORY[$LNUM1]}
+	VNUM2=${MEMORY[$LNUM2]}
+
+	if [ $OPCODE -eq 1 ]; then
+		VRES=$(($VNUM1 + $VNUM2))
+		# info "adding $VNUM1 + $VNUM2 = $VRES"
+		MEMORY[$LRES]=$VRES
+	elif [ $OPCODE -eq 2 ]; then
+		VRES=$(($VNUM1 * $VNUM2))
+		# info "multi $VNUM1 * $VNUM2 = $VRES"
+		MEMORY[$LRES]=$VRES
+	else
+		fatal "Unknown opcode $OPCODE"
+	fi
+
+	# declare -p MEMORY
+}
+
+run_all_programs() {
+	reset_memory
+
+	PROGRAM=0
+	while [ ${MEMORY[$PROGRAM]} -ne 99 ]; do
+		OPCODE=${MEMORY[$PROGRAM]}
+		INUM1=$(($PROGRAM + 1))
+		INUM2=$(($PROGRAM + 2))
+		IRES=$(($PROGRAM + 3))
+
+		run_program $OPCODE $INUM1 $INUM2 $IRES
+
+		PROGRAM=$(($PROGRAM + 4))
+	done
 }
 
 run_main() {
@@ -34,35 +93,7 @@ run_main() {
 	CODES[1]=12
 	CODES[2]=2
 
-	PROGRAM=0
-	while [ ${CODES[$PROGRAM]} -ne 99 ]; do
-		OPCODE=${CODES[$PROGRAM]}
-
-		INUM1=$(($PROGRAM + 1))
-		INUM2=$(($PROGRAM + 2))
-		IRES=$(($PROGRAM + 3))
-
-		LNUM1=${CODES[$INUM1]}
-		LNUM2=${CODES[$INUM2]}
-		LRES=${CODES[$IRES]}
-
-		VNUM1=${CODES[$LNUM1]}
-		VNUM2=${CODES[$LNUM2]}
-
-		if [ $OPCODE -eq 1 ]; then
-			VRES=$(($VNUM1 + $VNUM2))
-			info "Operation 'add': $VNUM1 + $VNUM2 = $VRES"
-			CODES[$LRES]=$VRES
-		elif [ $OPCODE -eq 2 ]; then
-			VRES=$(($VNUM1 * $VNUM2))
-			info "Operation 'mult': $VNUM1 * $VNUM2 = $VRES"
-			CODES[$LRES]=$VRES
-		else
-			fatal "Unknown opcode $OPCODE"
-		fi
-
-		PROGRAM=$(($PROGRAM + 4))
-	done
+	run_all_programs
 
 	VRES0=${CODES[0]}
 	info "Program done; Value of pos(0) = '$VRES0'"
---
+++ test/star3.test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Imports
+source ./_shared/logger.sh
+source ./stars/star3.sh 0
+
+# Variables
+# -none-
+
+test_RunAllPrograms_WithData_WorksOut() {
+	# Arrange
+	DATA=$1
+	EXP=$2
+	parse_codes $DATA
+
+	# Act
+	run_all_programs
+
+	# Assert
+	if [ ${MEMORY[0]} -ne $EXP ]; then
+		fatal "Result does not match $EXP instead is ${MEMORY[0]}"
+	fi
+}
+
+run_main() {
+	DATA1="1,9,10,3,2,3,11,0,99,30,40,50"
+	info "test_RunAllPrograms_WithData_WorksOut '$DATA1' 3500"
+	test_RunAllPrograms_WithData_WorksOut $DATA1 3500
+
+	DATA2="1,0,0,0,99"
+	info "test_RunAllPrograms_WithData_WorksOut '$DATA2' 2"
+	test_RunAllPrograms_WithData_WorksOut $DATA2 2
+
+	DATA3="2,3,0,3,99"
+	info "test_RunAllPrograms_WithData_WorksOut '$DATA3' 2"
+	test_RunAllPrograms_WithData_WorksOut $DATA3 2
+
+	DATA4="2,4,4,5,99,0"
+	info "test_RunAllPrograms_WithData_WorksOut '$DATA4' 2"
+	test_RunAllPrograms_WithData_WorksOut $DATA4 2
+
+	DATA5="1,1,1,4,99,5,6,0,99"
+	info "test_RunAllPrograms_WithData_WorksOut '$DATA5' 30"
+	test_RunAllPrograms_WithData_WorksOut $DATA5 30
+}
+
+case "$1" in
+	"run") run_main;;
+esac
