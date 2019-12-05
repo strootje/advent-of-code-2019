17344fa9149081a639d504c4b56bda992c92274b
Bas Stroosnijder <b.stroosnijder@cargoguide.com>
2019-12-05 10:50:33 +0100

    optimized code :)

--- stars/star5.sh
+++ stars/star5.sh
@@ -39,7 +39,6 @@ set_start_pos() {
 get_raw_wires() {
 	RAW=$1
 
-	IFS=","
 	while read -r wire; do
 		WIRE_NAME=$(new_wire_name)
 		WIRE_INDEX=${#WIRES[@]}
@@ -50,6 +49,7 @@ get_raw_wires() {
 		PREV_X=$START_X
 		PREV_Z=$START_Z
 
+		IFS=","
 		# shellcheck disable=SC2068
 		for move in ${wire[@]}; do
 			MOVE_DIR=${move:0:1}
@@ -80,9 +80,9 @@ get_raw_wires() {
 			PREV_X=$NEW_X
 			PREV_Z=$NEW_Z
 		done
+		unset IFS
 
 	done <<< "$RAW"
-	unset IFS
 }
 
 validate_wire() {
@@ -244,6 +244,8 @@ calculate_closests_intersection_to_start() {
 
 	INTERSECTION_DISTANCE=0
 
+	declare -A CHECKED=()
+
 	WIRE1_INDEX=0
 	while [ "$WIRE1_INDEX" -lt $WIRE1_LINES ]; do
 		WIRE1_BEGIN_X=$(get_wire_line_stat "$WIRE1_COLOR" "$WIRE1_INDEX" "x1")
@@ -253,23 +255,34 @@ calculate_closests_intersection_to_start() {
 
 		WIRE2_INDEX=0
 		while [ "$WIRE2_INDEX" -lt $WIRE2_LINES ]; do
-			trace "Checking Wire '$WIRE1_COLOR':'$WIRE1_INDEX' to '$WIRE2_COLOR':'$WIRE2_INDEX'"
-			WIRE2_BEGIN_X=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "x1")
-			WIRE2_BEGIN_Z=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "z1")
-			WIRE2_FINAL_X=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "x2")
-			WIRE2_FINAL_Z=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "z2")
+			KEY="$WIRE1_INDEX,$WIRE2_INDEX"
+			KEY_REVERSE="$WIRE2_INDEX,$WIRE1_INDEX"
 
-			INTERSECTION=$(calculate_intersection "$WIRE1_BEGIN_X" "$WIRE1_BEGIN_Z" "$WIRE1_FINAL_X" "$WIRE1_FINAL_Z" "$WIRE2_BEGIN_X" "$WIRE2_BEGIN_Z" "$WIRE2_FINAL_X" "$WIRE2_FINAL_Z")
-			INTERSECT_X=${INTERSECTION%%,*}
-			INTERSECT_Z=${INTERSECTION##*,}
+			IS_CHECKED=${CHECKED[KEY]}
+			if [ "$IS_CHECKED" == "1" ]; then
+				trace "Skipping Wire '$WIRE1_COLOR':'$WIRE1_INDEX' to '$WIRE2_COLOR':'$WIRE2_INDEX'"
+			else
+				trace "Checking Wire '$WIRE1_COLOR':'$WIRE1_INDEX' to '$WIRE2_COLOR':'$WIRE2_INDEX'"
+				WIRE2_BEGIN_X=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "x1")
+				WIRE2_BEGIN_Z=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "z1")
+				WIRE2_FINAL_X=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "x2")
+				WIRE2_FINAL_Z=$(get_wire_line_stat "$WIRE2_COLOR" "$WIRE2_INDEX" "z2")
 
-			if [ -n "$INTERSECTION" ]; then
-				DISTANCE_TO_START=$(calculate_manhattan "$INTERSECT_X" "$INTERSECT_Z" "$START_X" "$START_Z")
-				trace "Intersection at ($INTERSECT_X, $INTERSECT_Z) -> $DISTANCE_TO_START"
+				INTERSECTION=$(calculate_intersection "$WIRE1_BEGIN_X" "$WIRE1_BEGIN_Z" "$WIRE1_FINAL_X" "$WIRE1_FINAL_Z" "$WIRE2_BEGIN_X" "$WIRE2_BEGIN_Z" "$WIRE2_FINAL_X" "$WIRE2_FINAL_Z")
+				INTERSECT_X=${INTERSECTION%%,*}
+				INTERSECT_Z=${INTERSECTION##*,}
 
-				if [ "$INTERSECTION_DISTANCE" -eq 0 ] || [ "$DISTANCE_TO_START" -lt "$INTERSECTION_DISTANCE" ]; then
-					INTERSECTION_DISTANCE=$DISTANCE_TO_START
+				if [ -n "$INTERSECTION" ]; then
+					DISTANCE_TO_START=$(calculate_manhattan "$INTERSECT_X" "$INTERSECT_Z" "$START_X" "$START_Z")
+					trace "Intersection at ($INTERSECT_X, $INTERSECT_Z) -> $DISTANCE_TO_START"
+
+					if [ "$INTERSECTION_DISTANCE" -eq 0 ] || [ "$DISTANCE_TO_START" -lt "$INTERSECTION_DISTANCE" ]; then
+						INTERSECTION_DISTANCE=$DISTANCE_TO_START
+					fi
 				fi
+
+				CHECKED[$KEY]=1
+				CHECKED[$KEY_REVERSE]=1
 			fi
 
 			WIRE2_INDEX=$((WIRE2_INDEX + 1))
@@ -298,8 +311,8 @@ run_main() {
 	info "Loading wires into memory"
 	get_raw_wires "$RAW"
 
-	# DISTANCE=$(calculate_closests_intersection_to_start "RED" "GREEN")
-	# info "Distance='$DISTANCE'"
+	DISTANCE=$(calculate_closests_intersection_to_start "RED" "GREEN")
+	info "Distance='$DISTANCE'"
 }
 
 case "$1" in
